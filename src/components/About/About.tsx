d54c831b9d8db86e75713238a9a9a01a6cd4d419
Karim Dahoumane <krmdahoumane@gmail.com>
2023-07-24 21:41:33 +0200

    💬 chore(about): Modify text

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -5,20 +5,22 @@ const About = () => {
     <section className="bg-secondery text-white px-5 py-32" id="about">
       <div className="container mx-auto grid md:grid-cols-2 items-center justify-center md:justify-between">
         <div className="about-info">
-          <div className="text-4xl font-bold mb-5 border-b-[5px] w-[180px] border-rose-600 pb-2">
+          <div className="text-4xl font-bold mb-5 border-b-[5px] w-full border-rose-600 pb-2">
             About Me
           </div>
-          <p className="pb-5">
-            Hello, my name is Karim Dahoumane. <br />
-            I have recently graduated after completing two bachelor's degrees in Computer Science and a Master's in Web Engineering.
-            Over the last three years, I gained valuable experience through a 3-year apprenticeship in 3 different companies.
-          </p>
-          <p className="pb-5">
-            Throughout my academic projects and professional experiences,
-            I have worked with various programming languages such as Java and JavaScript,
-            as well as several frameworks including Spring and Vue. However,
-            I am most proficient in JavaScript, with expertise in React, NestJS, Node.js, and Express.js. Additionally, I am skilled in Tailwind CSS.
-          </p>
+          <div className="text-xl mb-5 pb-2">
+            <p className="pb-5">
+              Hello, my name is Karim Dahoumane. <br />
+              I have recently graduated after completing two bachelor's degrees in Computer Science and a Master's in Web Engineering.
+              Over the last three years, I gained valuable experience through a 3-year apprenticeship in 3 different companies with a different mission for each.
+            </p>
+            <p className="pb-5">
+              Throughout my academic projects and professional experiences,
+              I have worked with various programming languages such as Java and JavaScript,
+              as well as several frameworks including Spring and Vue. However,
+              I am most proficient in JavaScript, with technologies like React, NestJS, Node.js, and Express.js.
+            </p>
+          </div>
         </div>
 
         <div className="about-img">
@@ -26,6 +28,7 @@ const About = () => {
             src={PictureGif}
             alt="Gif of a hand coding"
             className="lgw-[80%] md:ml-auto"
+            loading="lazy"
           />
           {/* <a href="https://storyset.com/work">Work illustrations by Storyset</a> */}
         </div>
