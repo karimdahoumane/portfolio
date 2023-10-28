17a680684153372350c70daf97c47dc4b035116d
Karim Dahoumane <krmdahoumane@gmail.com>
2023-10-28 19:04:54 +0200

    💬 fix(contact): Remove text

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -6,21 +6,17 @@ const Contact = () => {
   return (
     <section className="bg-secondery px-5 py-32" id="contact">
       <div className="text-center md:w-[60%] mx-auto text-white">
-        <h2 className="text-4xl font-bold mb-5 border-b-[5px] w-[200px] mx-auto border-rose-600 pb-2">
+        <h2 className="text-4xl font-bold mb-8 border-b-[5px] w-[200px] mx-auto border-rose-600">
           Contact Me
         </h2>
-        <p>
+        <p className="pb-4">
           I am currently available for a full-time Fullstack Developer position.
           If you would like to discuss this opportunity further, please feel free to reach out to me via email, phone call, or LinkedIn.
         </p>
-
-        <p className="py-2">
+        <p>
           <span className="font-bold">Email:</span> krmdahoumane@gmail.com
         </p>
-        <p className="py-2">
-          <span className="font-bold">Phone:</span> +33 7 79 85 47 83
-        </p>
-        <p className="py-2">
+        <p>
           <span className="font-bold">LinkedIn:</span>{" "}
           <a
             href="https://www.linkedin.com/in/karimdahoumane/"
@@ -30,7 +26,6 @@ const Contact = () => {
           >
             <LinkedInIcon className="inline-block" /> Karim Dahoumane
           </a>
-
         </p>
       </div>
     </section>
